a1be4fcc6f2f4b729e7629fbd2737d3e629f45ee
Elektropay <richard.r@everpayinc.com>
2023-05-30 20:32:28 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import AboutHeroSection from "../components/AboutHeroSection/index";
-import AboutCareerSection from "../components/AboutCareerSection/index";
-import AboutInfoSection from "../components/AboutInfoSection";
-import { aboutObjOne, aboutObjThree, aboutObjTwo } from "../components/AboutInfoSection/Data";
-import AboutAwardsSection from "../components/AboutAwardsSection/index";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
-const About = () => {
+const Pricing = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -16,14 +12,13 @@ const About = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+<div>
+<H1> Pricing</h1>
+</div>
+
       <AboutHeroSection />
-      <AboutInfoSection {...aboutObjOne} />
-      <AboutInfoSection {...aboutObjTwo} />
-      <AboutInfoSection {...aboutObjThree} /> 
-      <AboutAwardsSection />
-      <AboutCareerSection />
     </>
   );
 };
 
-export default About;
+export default Pricing;
