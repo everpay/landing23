83e36a9e86854ab5d9cd877c60e63a8e7b2181a9
Elektropay <richard.r@everpayinc.com>
2023-06-04 04:59:33 -0500

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import ContactHeroSection from "../components/ContactHeroSection/index";
-import Contact from "../components/Contact";
 
 
 const ContactUsPage = () => {
